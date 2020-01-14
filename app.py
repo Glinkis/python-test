7a3af4741c2f660f3d0235a5ce1a0a59954dcf12
Victor Glindås <glinkis@gmail.com>
2020-01-14 20:25:49 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+def mul(a, b):
+    return a * b
+
+
+def div(a, b):
+    return a / b
+
+
+print(add(2, 2), sub(2, 2), mul(2, 2), div(2, 2))
+
+
